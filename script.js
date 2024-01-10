abbd3b87f1b27267d5700a848e5c099f1f7d971a
priyanka387 <priyankapathak222@gmail.com>
2024-01-10 23:21:53 +0530

    backend part1

---
+++ script.js
@@ -0,0 +1,11 @@
+// var a = 12;
+// var b = 123456;
+// var c = 45;
+
+// module.exports = {first:a, second:b};
+// console.log("hello");
+
+var a = 12;
+// console.log(a);
+
+module.exports = a;
---
+++ script1.js
@@ -0,0 +1,43 @@
+// var data =  require("./script.js");
+// console.log(data);
+
+// var oneLinerJoke = require('one-liner-joke');
+
+// console.log(oneLinerJoke.getRandomJoke());
+
+// var figlet = require("figlet");
+
+// figlet("Priyanka", function (err, data) {
+//     if (err) {
+//       console.log("Something went wrong...");
+//       console.dir(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+
+const express = require('express')
+const app = express()
+
+app.use(function(req, res, next) {
+    console.log("hello from middleware");
+    next();
+});
+
+app.use(function(req, res, next) {
+    console.log("hello from middleware 2");
+    next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/profile', function (req, res) {
+    res.send('Hello priyanka')
+  })
+
+app.listen(3000)
+
+
+
